c48b334e144838c09e1db4d1707d791f5b1a9b98
MochaFox <fox@mochafox.com>
2025-08-11 00:39:00 -0500

    fluid tweaks

--- src/main/java/com/mochafox/gatorade/fluid/custom/GatoradeFluid.java
+++ src/main/java/com/mochafox/gatorade/fluid/custom/GatoradeFluid.java
@@ -11,12 +11,17 @@ import com.mochafox.gatorade.item.ModItems;
 import com.mochafox.gatorade.item.custom.GatoradeBucketItem;
 
 import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.item.BucketItem;
 import net.minecraft.world.level.block.LiquidBlock;
+import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.material.Fluid;
 import net.minecraft.world.level.material.FluidState;
 import net.minecraft.world.level.material.MapColor;
+import net.minecraft.world.level.pathfinder.PathType;
+import net.minecraft.world.phys.Vec3;
 import net.neoforged.neoforge.common.SoundActions;
 import net.neoforged.neoforge.fluids.BaseFlowingFluid;
 import net.neoforged.neoforge.fluids.FluidType;
@@ -52,8 +57,16 @@ public abstract class GatoradeFluid extends BaseFlowingFluid {
             this.type = ModFluids.FLUID_TYPES.register(name, () -> new FluidType(FluidType.Properties.create()
                     .density(density).viscosity(viscosity).temperature(temperature)
                     .canHydrate(true).canConvertToSource(true)
-                    .supportsBoating(true).canSwim(true).fallDistanceModifier(0.0f)
+                    .supportsBoating(true)
+                    .fallDistanceModifier(0.0f).canSwim(true)
+                    .canExtinguish(true)
                     .lightLevel(0)
+                    .canSwim(true)
+                    .canPushEntity(true)
+                    .canDrown(true)
+                    .motionScale(0.0)
+                    .pathType(PathType.WATER)
+                    .adjacentPathType(PathType.WATER_BORDER)
                     .sound(SoundActions.BUCKET_FILL, SoundEvents.BUCKET_FILL)
                     .sound(SoundActions.BUCKET_EMPTY, SoundEvents.BUCKET_EMPTY)
                     .sound(SoundActions.FLUID_VAPORIZE, SoundEvents.FIRE_EXTINGUISH)
@@ -66,13 +79,10 @@ public abstract class GatoradeFluid extends BaseFlowingFluid {
 
             this.block = ModBlocks.BLOCKS.registerBlock(name + "_block",
                     properties -> new LiquidBlock(source.get(), properties
-                            .mapColor(MapColor.WATER)
-                            .replaceable()
-                            .noCollission()
-                            .strength(100.0F)
-                            .noLootTable()
-                            .ignitedByLava()
-            ));
+                        .replaceable()
+                        .noOcclusion()
+                        .sound(SoundType.WET_SPONGE)
+                        .mapColor(MapColor.WATER)));
 
             this.bucket = ModItems.ITEMS.registerItem(name + "_bucket",
                     properties -> new GatoradeBucketItem(source.get(), properties.stacksTo(1)));
@@ -86,9 +96,6 @@ public abstract class GatoradeFluid extends BaseFlowingFluid {
                     .block(() -> block.get());
         }
 
-        /**
-         * Creates properties for the source fluid in this registry set.
-         */
         public BaseFlowingFluid.Properties createProperties() {
             return createLazyProperties();
         }
